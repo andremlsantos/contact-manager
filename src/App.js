80c4db0ad78e312aea8f4a15d6013ab553f2d068
andremlsantos <andremlsantos@gmail.com>
2020-05-02 17:02:51 +0200

    bootstraap
    added boottraap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Contact from "./components/Contact";
 import Header from "./components/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 class App extends Component {
@@ -8,17 +9,19 @@ class App extends Component {
         return (
             <div className="App">
                 <Header branding="Contact Manager" />
-                <Contact
-                    name="Andre Santos"
-                    email="andremlsantos@gmail.com"
-                    phone="555-555-5555"
-                />
-                <Contact
-                    name="Mr Erik"
-                    email="mrerik@gmail.com"
-                    phone="111-111-1111"
-                />
-                <Contact />
+                <div className="container">
+                    <Contact
+                        name="Andre Santos"
+                        email="andremlsantos@gmail.com"
+                        phone="555-555-5555"
+                    />
+                    <Contact
+                        name="Mr Erik"
+                        email="mrerik@gmail.com"
+                        phone="111-111-1111"
+                    />
+                    <Contact />
+                </div>
             </div>
         );
     }
