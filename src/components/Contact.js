c611fe1b85b1c483b586fe140b1208983f996c9e
andremlsantos <andremlsantos@gmail.com>
2020-05-02 17:39:39 +0200

    Fontawesome, Bind method
    Fontawesome
    Bind method no constructor

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,11 +2,23 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 export default class Contact extends Component {
+    state = {};
+
+    onShowClick() {
+        console.log(this.state);
+    }
+
     render() {
         const { name, email, phone } = this.props.contact;
         return (
             <div className="card card-body mb-3">
-                <h4>{name}</h4>
+                <h4>
+                    {name}
+                    <i
+                        className="fas fa-sort-down"
+                        onClick={this.onShowClick.bind(this)}
+                    ></i>
+                </h4>
                 <ul className="list-group">
                     <li className="list-group-item">Email: {email}</li>
                     <li className="list-group-item">Phone: {phone}</li>
