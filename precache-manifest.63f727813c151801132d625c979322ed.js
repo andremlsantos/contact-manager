8a009632d0519f2994432637187877b8b0dcbbf2
andremlsantos <andremlsantos@gmail.com>
2020-05-05 18:44:31 +0200

    Updates

---
+++ precache-manifest.63f727813c151801132d625c979322ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1609571b859cf9c7a69c1da19d19c14",
+    "url": "/contact-manager/index.html"
+  },
+  {
+    "revision": "7bd8cd9dbc85d9f55ef1",
+    "url": "/contact-manager/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "795df026b5346f17ab3b",
+    "url": "/contact-manager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "7bd8cd9dbc85d9f55ef1",
+    "url": "/contact-manager/static/js/2.8514f25c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contact-manager/static/js/2.8514f25c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "795df026b5346f17ab3b",
+    "url": "/contact-manager/static/js/main.897b6855.chunk.js"
+  },
+  {
+    "revision": "cdb523c06c1825fbec7c",
+    "url": "/contact-manager/static/js/runtime-main.85f8d23d.js"
+  }
+]);
