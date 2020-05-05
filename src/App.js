4a38a2a77a1074090b43e96327b9cd885fc1d3b1
andremlsantos <andremlsantos@gmail.com>
2020-05-05 15:59:47 +0200

    Router Setup
    About component added
    Links to:
    / -> Contacts.js
    /contact/add -> AddContact
    /about -> About

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Route, Switch } from "react-router";
 import Header from "./components/layout/Header";
 import Contacts from "./components/contacts/Contacts";
 import { Provider } from "./context";
-
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
 import AddContact from "./components/contacts/AddContact";
+import About from "./components/pages/About";
+
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends Component {
     render() {
         return (
             <Provider>
-                <div className="App">
-                    <Header branding="Contact Manager" />
-                    <div className="container">
-                        <AddContact />
-                        <Contacts />
+                <Router>
+                    <div className="App">
+                        <Header branding="Contact Manager" />
+                        <div className="container">
+                            <Switch>
+                                <Route exact path="/" component={Contacts} />
+                                <Route
+                                    exact
+                                    path="/contact/add"
+                                    component={AddContact}
+                                />
+                                <Route exact path="/about" component={About} />
+                            </Switch>
+                        </div>
                     </div>
-                </div>
+                </Router>
             </Provider>
         );
     }
--- src/components/contacts/Contacts.js
+++ src/components/contacts/Contacts.js
@@ -21,6 +21,10 @@ export default class Contacts extends Component {
 
                     return (
                         <React.Fragment>
+                            <h1 className="display-4 mb-2">
+                                <span className="text-danger">Contact</span>{" "}
+                                List
+                            </h1>
                             {contacts.map((contact) => (
                                 <Contact
                                     key={contact.id}
